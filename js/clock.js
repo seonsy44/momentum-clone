33c33531edadaaef23acb6edb715285f83615e38
seonsy44 <seonsy44@gmail.com>
2021-07-30 21:52:52 +0900

    Creating real-time clock

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector('h2#clock');
+
+function getClock(){
+   const date = new Date(),
+    hh = String(date.getHours()).padStart(2,'0'),
+    mm = String(date.getMinutes()).padStart(2,'0'),
+    ss = String(date.getSeconds()).padStart(2,'0');
+   clock.innerText = `${hh}:${mm}:${ss}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
