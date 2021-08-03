25038581d458d2a7a8e6119e2212cf4dbce545f2
seonsy44 <seonsy44@gmail.com>
2021-08-03 18:31:55 +0900

    Adding todos

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.querySelector('#todo-form'),
 toDoInput = toDoForm.querySelector('input'),
 toDoList = document.querySelector('#todo-list');
 
+function paintToDo(newToDo){
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    li.appendChild(span);
+    span.innerText = '⊹ ' + newToDo;
+    toDoList.appendChild(li)
+}
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = '';
+    paintToDo(newToDo);
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
