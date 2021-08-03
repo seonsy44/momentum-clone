bff87afad1ad1c84fe4174f8da4e6d3f24eca8bd
seonsy44 <seonsy44@gmail.com>
2021-08-03 18:54:20 +0900

    delete button

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.querySelector('#todo-form'),
 toDoInput = toDoForm.querySelector('input'),
 toDoList = document.querySelector('#todo-list');
 
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newToDo){
     const li = document.createElement('li');
     const span = document.createElement('span');
-    li.appendChild(span);
     span.innerText = '⊹ ' + newToDo;
+    const btn = document.createElement('button');
+    btn.innerText = '×';
+    btn.addEventListener('click', deleteToDo);
+    li.appendChild(span);
+    li.appendChild(btn);
     toDoList.appendChild(li)
 }
 
