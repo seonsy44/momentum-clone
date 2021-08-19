f4527329a539aec3d5e4913fb48f01eb117e528f
seonsy44 <seonsy44@gmail.com>
2021-08-19 23:58:24 +0900

    Update login-form

--- js/greeting.js
+++ js/greeting.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
+const main = document.querySelector('main');
 
 const HIDDEN_CLASSNAME = 'hidden';
 const USERNAME_KEY = 'username';
@@ -15,7 +16,7 @@ function onLoginSubmit(event){
 
 function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    main.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
